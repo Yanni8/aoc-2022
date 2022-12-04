980d09af3a1ff3aba42a1ec3ccdcfc8626c077d4
Yannick Mueller <yannick.mueller@fusiva.ch>
2022-12-04 08:35:41 +0100

    add day 4

---
+++ day-4/part1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include <string>
+
+#include <fstream>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Syntax: " << argv[0] << " <FileName>";
+    }
+
+    std::string line, pair1, pair2;
+
+    std::ifstream file(argv[1]);
+
+    unsigned int position,x1,x2,y1,y2, counter = 0;
+
+    while (std::getline(file, line))
+    {
+        position = line.find(",");
+        pair1 = line.substr(0, position);
+        pair2 = line.substr(position+1);
+        
+        position = pair1.find("-");
+        x1 = std::stoi(pair1.substr(0, position));
+        x2 = std::stoi(pair1.substr(position+1));
+        
+        position = pair2.find("-");
+        y1 = std::stoi(pair2.substr(0, position));
+        y2 = std::stoi(pair2.substr(position+1));
+        if(y1 >= x1 and x2 >= y2 or x1 >= y1 and y2 >=  x2 ){
+            counter++;
+        }
+    }
+
+    std::cout << "Number of lines: " << counter << std::endl;
+    
+}
